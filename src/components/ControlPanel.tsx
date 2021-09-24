8e93be489314f76c8f83e2ce35c98b9b63c8785f
Nicolas Steuernagle <nicosteu@udel.edu>
2021-09-23 20:17:15 -0400

    ability to shuffle users

--- src/components/ControlPanel.tsx
+++ src/components/ControlPanel.tsx
@@ -1,24 +1,38 @@
 import { Button, Col } from 'react-bootstrap';
 import { Card } from '../interfaces/card';
 import CARDS from '../assets/cards.json';
-import { getRandomElement } from '../utilities/data';
+import { getRandomElement, shuffle } from '../utilities/data';
 import { BlockLike } from 'typescript';
 import { UserList } from './UserList';
-
+import { useState } from 'react';
+import { Task as User} from 'editable-dnd-list';
 
 
 export function ControlPanel({setCard, reveal, answerRevealed, reveal2, hintRevealed}: {setCard: (c: Card)=>void, reveal: (r: boolean) => void, reveal2:(h: boolean) => void, answerRevealed: boolean, hintRevealed: boolean}): JSX.Element{
+    const [users, setUsers] = useState<Task[]>([
+        {id: '1', text: "Nick"}
+        {id: '2', text: "Peanut"}
+        {id: '3', text: "Bandit"}
+        {id: '4', text: "Bella"}
+    ]);
+    
     function setRandomCard(){
         reveal(false);
         reveal2(false);
         setCard(getRandomElement(CARDS as Card[]))
     }
 
+    function shuffleUsers(){
+        let shuffledUsers: User[] = shuffle(users)
+        setUsers([...shuffledUsers]);
+    }
+
     return <Col>
         <h1>Control Panel</h1>
-        <UserList></UserList>
+        <UserList users = {users} setUsers = {setUsers}></UserList>
         <Button onClick = {setRandomCard} className="m-4">Swap Current Card</Button>
         <Button onClick = {() => reveal(!answerRevealed)}className="m-4">Reveal Answer</Button>
         <Button onClick = {() => reveal2(!hintRevealed)}className="m-4">Reveal Hint</Button>
+        <Button onClick = {shuffleUsers}className="m-4">Shuffle Users</Button>
     </Col>
 }
