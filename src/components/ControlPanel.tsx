2640dc21d58d4fe0818403177843fb0273e6ffc4
Nicolas Steuernagle <nicosteu@udel.edu>
2021-09-23 20:37:25 -0400

    able to save with local storage

--- src/components/ControlPanel.tsx
+++ src/components/ControlPanel.tsx
@@ -7,14 +7,26 @@ import { UserList } from './UserList';
 import { useState } from 'react';
 import { Task, Task as User} from 'editable-dnd-list';
 
+export const LOCAL_STORAGE_USERS = 'ta-trainer-users';
+
+export const INITIAL_USERS: User[] = [
+    {id: '1', text: "Nick"},
+    {id: '2', text: "Peanut"},
+    {id: '3', text: "Bandit"},
+    {id: '4', text: "Bella"}
+];
+
+export function getLocalStorageUsers(): User[] {
+    let rawUsers: string|null = localStorage.getItem(LOCAL_STORAGE_USERS);
+    if(rawUsers === null){
+        return [...INITIAL_USERS];
+    }else{
+        return JSON.parse(rawUsers);
+    }
+}
 
 export function ControlPanel({setCard, reveal, answerRevealed, reveal2, hintRevealed}: {setCard: (c: Card)=>void, reveal: (r: boolean) => void, reveal2:(h: boolean) => void, answerRevealed: boolean, hintRevealed: boolean}): JSX.Element{
-    const [users, setUsers] = useState<Task[]>([
-        {id: '1', text: "Nick"},
-        {id: '2', text: "Peanut"},
-        {id: '3', text: "Bandit"},
-        {id: '4', text: "Bella"}
-    ]);
+    const [users, setUsers] = useState<User[]>(getLocalStorageUsers);
     
     function setRandomCard(){
         reveal(false);
@@ -27,6 +39,11 @@ export function ControlPanel({setCard, reveal, answerRevealed, reveal2, hintReve
         setUsers([...shuffledUsers]);
     }
 
+    function save(){
+        localStorage.setItem(LOCAL_STORAGE_USERS, JSON.stringify(users));
+
+    }
+
     return <Col>
         <h1>Control Panel</h1>
         <UserList users = {users} setUsers = {setUsers}></UserList>
@@ -34,5 +51,6 @@ export function ControlPanel({setCard, reveal, answerRevealed, reveal2, hintReve
         <Button onClick = {() => reveal(!answerRevealed)}className="m-4">Reveal Answer</Button>
         <Button onClick = {() => reveal2(!hintRevealed)}className="m-4">Reveal Hint</Button>
         <Button onClick = {shuffleUsers}className="m-4">Shuffle Users</Button>
+        <Button onClick = {save}className="m-4" variant = "success">Save</Button>
     </Col>
 }
